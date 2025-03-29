18f4993fbf6238a49d8ab3c8a74951483e38875a
Youbin Kim <youbin0210@naver.com>
2025-03-29 23:21:16 +0900

    try except문 추가

--- hw5/hw5_web.py
+++ hw5/hw5_web.py
@@ -15,31 +15,31 @@ while True:
     filename = filename[1].lstrip("/")
 
     # 웹 서버 코드 작성
-    if filename == 'index.html':
-        f = open(filename, 'r', encoding='utf-8')
-        mimeType = 'text/html'
+    try:
+        if filename == 'index.html':
+            f = open(filename, 'r', encoding='utf-8')
+            mimeType = 'text/html'
     
-    elif filename == 'iot.png':
-        f = open(filename, 'rb')
-        mimeType = 'image/png'
+        elif filename == 'iot.png':
+            f = open(filename, 'rb')
+            mimeType = 'image/png'
     
-    elif filename == 'favicon.ico':
-        f = open(filename, 'rb')
-        mimeType = 'image/x-icon'
+        elif filename == 'favicon.ico':
+            f = open(filename, 'rb')
+            mimeType = 'image/x-icon'
     
     # 각 객체(파일 또는 문자열) 전송 후, 소켓 닫기(c.close())
-    res_msg = 'HTTP/1.1 200 OK\r\nContent-Type: ' + mimeType + '\r\n\r\n'
-    c.send(res_msg.encode())
+        res_msg = 'HTTP/1.1 200 OK\r\nContent-Type: ' + mimeType + '\r\n\r\n'
+        c.send(res_msg.encode())
+        
+        if filename == 'index.html':
+            data = f.read()
+            c.send(data.encode('euc-kr'))
+        else:
+            data = f.read()
+            c.send(data)
     
-    if filename == 'index.html':
-        data = f.read()
-        c.send(data.encode('euc-kr'))
-    
-    elif filename == 'iot.png' or filename == 'favicon.ico':
-        data = f.read()
-        c.send(data)
-    
-    else:
+    except:        
         res_msg = 'HTTP/1.1 404 Not Found\r\n\r\n<HTML><HEAD><TITLE>Not Found</TITLE></HEAD><BODY>Not Found</BODY></HTML>'
         c.send(res_msg.encode())
     
